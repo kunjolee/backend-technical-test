acdcd5dfd47c3d614a4cd89a0e3cddb86cd386fc
kunjolee <josuelee28@gmail.com>
2025-02-28 11:39:52 -0600

    refactor: updated the server to use new database class

--- src/infrastructure/http/server/server.ts
+++ src/infrastructure/http/server/server.ts
@@ -3,7 +3,11 @@ import eventsRoutes from '../routes/eventRoutes'
 
 import { env } from '../../config/env'
 import type { ServerRoutes, PORT } from './types/serverTypes'
+import sequelize, { database } from '../../database/connection'
 
+/**
+ * Class representing the server.
+ */
 class Server {
   private app: Application
   private port: PORT
@@ -16,14 +20,39 @@ class Server {
       events: '/api/events'
     }
 
+    this.dbConnection()
+    this.middleware()
     this.routes()
   }
 
-  routes() {
+  /**
+   * Establishes the database connection.
+   *
+   * @returns {Promise<void>}
+   */
+  async dbConnection(): Promise<void> {
+    database.connect()
+    database.sync()
+  }
+
+  /**
+   * Sets up middleware for the server.
+   */
+  middleware(): void {
+    this.app.use(express.json())
+  }
+
+  /**
+   * Sets up routes for the server.
+   */
+  routes(): void {
     this.app.use(this.path.events, eventsRoutes)
   }
 
-  public listen() {
+  /**
+   * Starts the server and listens on the specified port.
+   */
+  public listen(): void {
     this.app.listen(this.port, () => {
       console.log(`Server running at http://localhost:${this.port}`)
       console.log(`Visit: http://localhost:${this.port}${this.path.events}`)
