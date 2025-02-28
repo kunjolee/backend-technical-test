274b1f220155f67af2af852bf823050f4f01e1e0
kunjolee <josuelee28@gmail.com>
2025-02-28 11:27:40 -0600

    fix: use case event type

--- src/application/use-cases/types/index.ts
+++ src/application/use-cases/types/index.ts
@@ -1,10 +1,10 @@
 export interface EventType {
-  id: number | null
+  id?: number | null
   name: string
   description: string
   date: Date
   location: string
   organizer: string
-  createdAt: Date | null
-  updatedAt: Date | null
+  createdAt?: Date | null
+  updatedAt?: Date | null
 }
