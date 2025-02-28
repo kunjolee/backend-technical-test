b02cf7a05dcb6379ee0a0a8c1e956c49d53e92b2
kunjolee <josuelee28@gmail.com>
2025-02-28 12:56:45 -0600

    refactor: update event controller  and routes for better organization and consistency

--- src/infrastructure/http/routes/eventRoutes.ts
+++ src/infrastructure/http/routes/eventRoutes.ts
@@ -1,42 +1,37 @@
 import { Router } from 'express'
-import {
-  createEvent,
-  updateEvent,
-  deleteEvent,
-  getEvents,
-  getEventById
-} from '../controllers/eventController'
+import { EventController } from '../controllers/eventController'
 
 const router = Router()
+const eventController = new EventController()
 
 /**
  * @route GET /events
  * @description Retrieves all events
  */
-router.get('/', getEvents)
+router.get('/', (req, res) => eventController.getEvents(req, res))
 
 /**
  * @route GET /events/:id
  * @description Retrieves an event by its ID
  */
-router.get('/:id', getEventById)
+router.get('/:id', (req, res) => eventController.getEventById(req, res))
 
 /**
  * @route POST /events
  * @description Creates a new event
  */
-router.post('/', createEvent)
+router.post('/', (req, res) => eventController.createEvent(req, res))
 
 /**
  * @route PUT /events/:id
  * @description Updates an existing event
  */
-router.put('/:id', updateEvent)
+router.put('/:id', (req, res) => eventController.updateEvent(req, res))
 
 /**
  * @route DELETE /events/:id
  * @description Deletes an event
  */
-router.delete('/:id', deleteEvent)
+router.delete('/:id', (req, res) => eventController.deleteEvent(req, res))
 
 export default router
