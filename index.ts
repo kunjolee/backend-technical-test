b7a02d59ae80de3e16f004d64fb7ea1b6852087c
kunjolee <josuelee28@gmail.com>
2025-02-27 19:31:04 -0600

    feat: dotenv for environment variables

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello World!' })
