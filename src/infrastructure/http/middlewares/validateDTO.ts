fc3102f14622f07259dc9aa182556b379d21ae2d
kunjolee <josuelee28@gmail.com>
2025-03-01 01:04:08 -0600

    refactor: make dto middleware generic and enforce allowed fields

--- src/infrastructure/http/middlewares/validateDTO.ts
+++ src/infrastructure/http/middlewares/validateDTO.ts
@@ -1,21 +1,45 @@
 import { Request, Response, NextFunction } from 'express'
-import { CreateEventDTO } from '../../../application/dtos/createEventDTO'
+import { EVENT_ALLOWED_BODY_FIELDS } from '../../../application/constants/eventConstants'
+
+/**
+ * Interface representing a validatable Data Transfer Object (DTO).
+ */
+interface ValidatableDTO {
+  new (...args: any[]): { validate: () => string[] }
+}
 
 /**
  * Middleware to validate a Data Transfer Object (DTO).
  *
- * @param {typeof CreateEventDTO} dtoClass - The DTO class to validate.
+ * @param {ValidatableDTO} dtoClass - The DTO class to validate.
  * @returns {(req: Request, res: Response, next: NextFunction) => void} Middleware function to validate the DTO.
  */
-export function validateDTO(dtoClass: typeof CreateEventDTO) {
+export function validateDTO(dtoClass: ValidatableDTO) {
   return (req: Request, res: Response, next: NextFunction) => {
+    // Extract the fields from the request body
+    const bodyFields = Object.keys(req.body)
+    // Identify any invalid fields that are not allowed in the DTO
+    const invalidFields = bodyFields.filter(
+      (field) => !EVENT_ALLOWED_BODY_FIELDS.includes(field)
+    )
+
+    if (invalidFields.length > 0) {
+      // If there are invalid fields, respond with a 400 status and the invalid fields
+      res.status(400).json({
+        status: 400,
+        message: `Invalid field(s): ${invalidFields.join(', ')}`,
+        error: 'Bad Request'
+      })
+      return
+    }
+
     // Create an instance of the DTO class with the request body
     const dto = new dtoClass(
       req.body.name,
-      req.body.description || null,
+      req.body.description,
       req.body.date,
       req.body.location,
-      req.body.organizer || null
+      req.body.organizer
     )
 
     // Validate the DTO instance
